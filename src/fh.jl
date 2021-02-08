dd63251cdb506e91a2a5ac3ad51595fb355ac9be
Volkan Aydıngül <volkanaydingul@gmail.com>
2021-02-08 23:08:57 +0300

    Add Base.show()

--- src/fh.jl
+++ src/fh.jl
@@ -13,3 +13,19 @@ end
 # Callable object definitions
 (f::FunctionHolder)(args...) = f.f(args..., f.pos_args...; f.key_args...)
 (f::FunctionHolder)() = f.f(f.pos_args...; f.key_args...)
+
+
+function show(io::IO, fh::NetworkData)
+
+	println("\nFunction Holder:")
+	println("Function => ", fh.f)
+	println("Positional Arguments => ", fh.pos_args)
+	println("Keyword Arguments => ", fh.key_args, "\n")
+	
+	end
+	
+	function show(io::IO, ::MIME"text/plain", nd::NetworkData{T}) where T <: AbstractStatus
+		
+		show(io, d)
+	
+	end
